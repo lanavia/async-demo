4e9123176750de865bafa976f9f3b2bcea267c9e
Your Name <your.email@example.com>
2024-02-22 20:32:21 -0300

    use of Promise

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 console.log('Before')
-getUser(1, function (user) {
+/* getUser(1, function (user) {
     getRepositories(user.gitHubUsername, (repositories) => {
         console.log(repositories)
+        getCommits(repositories[0], (commits) => {
+            console.log(commits)
+        })
     })
-})
+}) */
 console.log('After')
 
+getUser(1)
+    .then(user => getRepositories(user.gitHubUsername))
+    .then(repos => getCommits(repos[0]))
+    .then(commits => console.log('Commits ', commits))
+    .catch(err => console.log("Error", err.message))
 
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from DB ..')
-        callback({ id: id, gitHubUsername: 'lanavia' })
+function getUser(id) {
+    return new Promise((resolve, reject) => {
 
-    }, 2000)
-    return 1
+        setTimeout(() => {
+            console.log('Reading a user from DB ..')
+            resolve({ id: id, gitHubUsername: 'lanavia' })
+
+        }, 2000)
+    })
 
 }
 
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        console.log("getting GitHub Repositories")
-        callback(['repo1', 'repo2', 'repo3'])
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("getting GitHub Repositories")
+            resolve(['repo1', 'repo2', 'repo3'])
 
-    }, 2000)
+        }, 2000)
+    })
+}
+
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("getting GitHub Repositories")
+            resolve(['repo1', 'repo2', 'repo3'])
+
+        }, 2000)
+    })
 }
